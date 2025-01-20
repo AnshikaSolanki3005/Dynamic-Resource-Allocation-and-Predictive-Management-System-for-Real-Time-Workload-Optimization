d6e3bee24fdd50ec322ae0379f923e8c6104a331
Anshika Solanki <anshikasolanki0430@gmail.com>
2025-01-20 15:57:26 +0530

    Adding the data_ingesion.py file

--- src/components/data_ingesion.py
+++ src/components/data_ingesion.py
@@ -11,7 +11,7 @@ from dataclasses import dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"Rawdata.csv")
+    raw_data_path: str=os.path.join('artifacts',"new_df.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -20,7 +20,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Enter the data ingestion method or component")
         try:
-            df=pd.read_csv('notebooks\data.csv')
+            df=pd.read_csv('notebooks\new_df.csv')
             
             
             logging.info('Read the dataframe')
@@ -39,8 +39,8 @@ class DataIngestion:
 
 
             return
-                 self.ingestion_config.train_data_path,
-                 self.x=global()
+            self.ingestion_config.train_data_path,
+            self.x=global()
 
         catch:
             pass
