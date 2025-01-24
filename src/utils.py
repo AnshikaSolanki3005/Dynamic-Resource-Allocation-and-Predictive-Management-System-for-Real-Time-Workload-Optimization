1b25e1a735591ad38e2779c15f317a7d7bc8cf85
Anshika Solanki <anshikasolanki0430@gmail.com>
2025-01-24 22:25:27 +0530

    utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -25,13 +26,26 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
         for i in range (len(list(models))):
             model=list(models.value())[i]
+            para=param[list(model.values())[i]]
 
+            gs=GridSearchCV(model,para,cv=cv,n_jobs=n_jobs,verbose=verbose,refit=refit)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
             train_model_score=r2_score(y_train,y_train_pred)
             train_model_score=r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]]=test_model_score
 
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
