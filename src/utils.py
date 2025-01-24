669becaf82fe23a18a11a4806edee8b13e9f4b33
Anshika Solanki <anshikasolanki0430@gmail.com>
2025-01-24 15:32:19 +0530

    utils.py

---
+++ src/utils.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import dill
+
+from sklearn.metrics import r2_score
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        X_train, X_test, y_train, y_test=train_test_split(
+            X,y,test_size=0.2,random_state=42
+        )
+
+        report={}
+
+        for i in range (len(list(models))):
+            model=list(models.value())[i]
+
+            model.fit(X_train,y_train)
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            train_model_score=r2_score(y_train,y_train_pred)
+            train_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.keys())[i]]=test_model_score
+
+    except Exception as e:
+        raise CustomException(e,sys)
